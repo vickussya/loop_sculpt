3dffc5db194b8e8f674c20bdc32c5ffb8e75629a
vickussya <victoriastoyanova006@gmail.com>
2026-02-16 18:34:02 +0200

    Update add-on author metadata

--- loop_sculpt.py
+++ loop_sculpt.py
@@ -1,4 +1,14 @@
-﻿import bpy
+﻿bl_info = {
+    "name": "Loop Sculpt",
+    "author": "vickussya",
+    "version": (1, 0, 0),
+    "blender": (3, 6, 0),
+    "location": "View3D > Sidebar > Edit",
+    "description": "Dissolve edge loops quickly with a modal wheel-controlled tool",
+    "category": "Mesh",
+}
+
+import bpy
 import bmesh
 from bpy.types import Operator, Panel, PropertyGroup
 from bpy.props import (
