352084d249999887470523728561386a65941e66
vickussya <victoriastoyanova006@gmail.com>
2026-02-16 22:50:32 +0200

    Fix addon registration and line endings

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
     "name": "Loop Sculpt",
     "author": "vickussya",
     "version": (1, 0, 0),
-    "blender": (4, 0, 0),
+    "blender": (3, 6, 0),
     "location": "View3D > Sidebar",
     "description": "Loop selection and dissolve helper",
     "category": "Mesh",
@@ -10,5 +10,5 @@
 
 from . import loop_sculpt
 
-register = loop_sculpt.register()
-unregister = loop_sculpt.unregister()
+register = loop_sculpt.register
+unregister = loop_sculpt.unregister
